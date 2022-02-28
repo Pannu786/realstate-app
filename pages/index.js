083f273ad5020c10184fac9f56cff83502b6da13
Pannu786 <thepannu786@gmail.com>
2022-02-28 16:57:40 +0530

    added pic url and Banner cmp

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,65 @@
-import link from 'next/link';
+import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+const Banner = ({
+  purpose,
+  title1,
+  title2,
+  desc1,
+  desc2,
+  linkName,
+  buttonText,
+  imageUrl,
+}) => (
+  <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
+    <Image src={imageUrl} width={500} height={300} alt='banner' />
+    <Box p='5'>
+      <Text color='gray.500' fontSize='sm' fontWeight='medium'>
+        {purpose}
+      </Text>
+      <Text fontSize='3xl' fontWeight='bold'>
+        {title1}
+        <br />
+        {title2}
+      </Text>
+      <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>
+        {desc1}
+        <br />
+        {desc2}
+      </Text>
+      <Button fontSize='xl'>
+        <Link href={linkName}>
+          <a>{buttonText}</a>
+        </Link>
+      </Button>
+    </Box>
+  </Flex>
+);
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Box>
+      <Banner
+        purpose='Rent A HOME'
+        title1='Rental Homes For'
+        title2='Students'
+        desc1='We provide you with the best rental homes for students'
+        desc2='and more'
+        buttonText='Explore renting homes'
+        linkName='/search?purpose=for-rent'
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
+      />
+
+      <Banner
+        purpose='BUY A HOME'
+        title1='Find Homes For Your'
+        title2='Dream Homes'
+        desc1='Explore Apartments for sale'
+        desc2='and more'
+        buttonText='Explore Buying Homes'
+        linkName='/search?purpose=for-sale'
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
+      />
+    </Box>
   );
 }
